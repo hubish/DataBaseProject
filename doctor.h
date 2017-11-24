f635dab22425dd63a1fb2ad9406cec066dcba2e5
Владимир <Spieler@localhost.localdomain>
2017-11-24 12:45:22 +0300

    v1.0

--- doctor.h
+++ doctor.h
@@ -1,11 +1,17 @@
 #ifndef DOCTOR_H
 #define DOCTOR_H
+#include "human.h"
 
-
-class doctor
+class doctor:public human
 {
+private:
+    string status, experience, position;
+
 public:
     doctor();
+    doctor(doctor* doctor);
+    void saveIntoFile(string fileName);
+    doctor downloadFromFile(string fileName);
 };
 
 #endif // DOCTOR_H
--- patient.h
+++ patient.h
@@ -1,11 +1,23 @@
 #ifndef PATIENT_H
 #define PATIENT_H
+#include "string.h"
+#include "human.h"
 
 
-class patient
+class patient:public human
 {
+private:
+    string diagnosis[], masculine[];
+
 public:
     patient();
+    patient(patient* patient);
+    void saveIntoFile(string fileName);
+    patient downloadFromFile(string fileName);
+    void setDiagnosis(string diagnosis[]);
+    void changeDiagnosis(string diagnosis[]);
+    void setMasculine(string masculine[]);
+    void changeMasculine(string masculine[]);
 };
 
 #endif // PATIENT_H
